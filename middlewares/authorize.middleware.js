2d72ddb5a1f5e76b154e414dafddd1dff7dcaa79
Raphael Ragoomundun <rragoomundun@protonmail.com>
2025-12-25 12:49:25 +0400

    Authorized

---
+++ middlewares/authorize.middleware.js
@@ -0,0 +1,27 @@
+import jwt from 'jsonwebtoken';
+import httpStatus from 'http-status-codes';
+
+import User from '../models/User.js';
+
+import ErrorResponse from '../classes/ErrorResponse.js';
+
+const authorize = async (req, res, next) => {
+  try {
+    const { token } = req.cookies;
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({ where: { id: decoded.id } });
+
+    req.user = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role
+    };
+
+    next();
+  } catch {
+    return next(new ErrorResponse('Unauthorized', httpStatus.UNAUTHORIZED));
+  }
+};
+
+export default authorize;
