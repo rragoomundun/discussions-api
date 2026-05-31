c463961771cb4f810d7032f17800e46ab38e1bd4
Raphael Ragoomundun <rragoomundun@protonmail.com>
2026-05-31 14:48:25 +0400

    feat: add POST /message endpoint to post a message in a discussion

--- controllers/message.controller.js
+++ controllers/message.controller.js
@@ -72,4 +72,51 @@ const getMessagesInDiscussion = async (req, res, next) => {
   res.status(httpStatus.OK).json(result);
 };
 
-export { getMessagesInDiscussion };
+/**
+ * @api {POST} /message Post Message
+ * @apiGroup Message
+ * @apiName MessagePostMessage
+ *
+ * @apiDescription Post a message in a discussion.
+ *
+ * @apiBody {String} message The message content.
+ * @apiBody {Number} discussionId The discussion id.
+ *
+ * @apiParamExample {json} Body Example
+ * {
+ *   "message": "Hello world!",
+ *   "discussionId": 1
+ * }
+ *
+ * @apiSuccess (Success (201)) {Number} id The message id
+ * @apiSuccess (Success (201)) {String} message The message content
+ * @apiSuccess (Success (201)) {Date} date The message date
+ * @apiSuccess (Success (201)) {Object} author The message author
+ * @apiSuccess (Success (201)) {Number} author.id The author id
+ * @apiSuccess (Success (201)) {String} author.name The author name
+ * @apiSuccess (Success (201)) {String} author.image The author avatar
+ * @apiSuccess (Success (201)) {String} author.signature The author signature
+ *
+ * @apiError (Error (400)) INVALID_PARAMETERS One or more parameters are invalid
+ * @apiError (Error (401)) UNAUTHORIZED The user isn't logged in
+ *
+ * @apiPermission Private
+ */
+const postMessage = async (req, res, next) => {
+  const { message, discussionId } = req.body;
+  const { id: authorId } = req.user;
+
+  const [newMessage, author] = await Promise.all([
+    Message.create({ message, discussionId, authorId, date: new Date() }),
+    User.findOne({ where: { id: authorId }, attributes: ['id', 'name', 'image', 'signature'] })
+  ]);
+
+  res.status(httpStatus.CREATED).json({
+    id: newMessage.id,
+    message: newMessage.message,
+    date: newMessage.date,
+    author: { id: author.id, name: author.name, image: author.image, signature: author.signature }
+  });
+};
+
+export { getMessagesInDiscussion, postMessage };
--- validators/message.validator.js
+++ validators/message.validator.js
@@ -1,9 +1,27 @@
-import { query } from 'express-validator';
+import { query, body } from 'express-validator';
 
 import validation from './validation.js';
+
+import Discussion from '../models/Discussion.js';
 
 const getMessagesInDiscussionValidator = validation([
   query('discussionId').notEmpty().withMessage('EMPTY').isInt().withMessage('INVALID')
 ]);
 
-export { getMessagesInDiscussionValidator };
+const postMessageValidator = validation([
+  body('message').notEmpty().withMessage('EMPTY'),
+  body('discussionId')
+    .notEmpty()
+    .withMessage('EMPTY')
+    .isInt()
+    .withMessage('INVALID')
+    .custom(async (value) => {
+      const discussion = await Discussion.findOne({ where: { id: value } });
+
+      if (!discussion) {
+        throw new Error('NOT_FOUND');
+      }
+    })
+]);
+
+export { getMessagesInDiscussionValidator, postMessageValidator };
