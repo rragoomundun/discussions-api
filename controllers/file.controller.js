421a892850581aeb3c8b6493fe3e17a5e50226c0
Raphael Ragoomundun <rragoomundun@protonmail.com>
2026-08-07 19:47:51 +0400

    Switched to S3 for file upload management.

--- controllers/file.controller.js
+++ controllers/file.controller.js
@@ -1,36 +1,66 @@
 import fs from 'fs/promises';
 import httpStatus from 'http-status-codes';
+import { S3Client, PutObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
 
 import ErrorResponse from '../classes/ErrorResponse.js';
 
-const __dirname = import.meta.dirname;
+const s3 = new S3Client({
+  region: process.env.AWS_S3_REGION,
+  credentials: {
+    accessKeyId: process.env.APP_ACCESS_KEY_ID,
+    secretAccessKey: process.env.APP_SECRET_ACCESS_KEY
+  }
+});
 
 /**
  * @api {POST} /file Upload
  * @apiGroup File
  * @apiName FileUpload
  *
- * @apiDescription Upload a file
+ * @apiDescription Upload a file to the S3 file bucket. Size is limited to 5 MB.
  *
  * @apiBody {File{5}} file The file to be uploaded.
  *
+ * @apiSuccessExample Success Example
+ * {
+ *   "link": "https://s3.eu-west-1.bucketname.com/user/1/1780509592231.jpeg",
+ *   "key": "user/1/1780509592231.jpeg"
+ * }
+
  * @apiError (Error (400)) INVALID_PARAMETERS The file parameter is invalid
+ * @apiError (Error (500)) UPLOAD_FAILED Cannot upload file
  *
  * @apiPermission Private
  */
 const uploadFile = async (req, res, next) => {
-  const path = req.file.path.split('public')[1];
-  res.status(httpStatus.OK).json({ path });
+  const { file } = req;
+  const params = {
+    Bucket: process.env.AWS_S3_FILES_BUCKET_NAME,
+    Key: `user/${req.user.id}/${Date.now()}.${file.mimetype.split('/')[1]}`,
+    Body: file.buffer,
+    ContentType: file.mimetype
+  };
+
+  try {
+    await s3.send(new PutObjectCommand(params));
+
+    res.status(httpStatus.OK).json({
+      link: `https://s3.${process.env.AWS_S3_REGION}.amazonaws.com/${params.Bucket}/${params.Key}`,
+      key: params.Key
+    });
+  } catch {
+    throw new ErrorResponse('Upload failed', httpStatus.INTERNAL_SERVER_ERROR, 'UPLOAD_FAILED');
+  }
 };
 
 /**
- * @api {POST} /file Delete
+ * @api {DELETE} /file Delete
  * @apiGroup File
  * @apiName FileDelete
  *
  * @apiDescription Delete a file
  *
- * @apiBody {String} path The file path.
+ * @apiBody {String} fileName The file path.
  *
  * @apiError (Error (400)) INVALID_PARAMETERS The file parameter is invalid
  * @apiError (Error (400)) NOT_OWNER The user isn't the owner of the file
@@ -38,26 +68,27 @@ const uploadFile = async (req, res, next) => {
  *
  * @apiPermission Private
  */
-const deleteFile = async (req, res, next) => {
-  const { path } = req.body;
+const deleteFile = async (req, res) => {
+  const { fileName } = req.body;
   const { user } = req;
 
-  if (user.role === 'regular' && path.includes(`/user/${user.id}`) === false) {
+  if (user.role === 'regular' && fileName.includes(`user/${user.id}`) === false) {
     return next(
       new ErrorResponse(`Cannot delete file that the user isn't the owner`, httpStatus.BAD_REQUEST, 'NOT_OWNER')
     );
   }
 
-  const pathStart = __dirname.split('controllers')[0];
-  const fullPath = `${pathStart}public${path}`;
+  const params = {
+    Bucket: process.env.AWS_S3_FILES_BUCKET_NAME,
+    Key: fileName
+  };
 
   try {
-    await fs.unlink(fullPath);
+    await s3.send(new DeleteObjectCommand(params));
+    res.status(httpStatus.OK).end();
   } catch {
-    return next(new ErrorResponse('Cannot delete file', httpStatus.INTERNAL_SERVER_ERROR, 'FAILED'));
+    throw new ErrorResponse('Cannot delete file', httpStatus.INTERNAL_SERVER_ERROR, 'FAILED');
   }
-
-  res.status(httpStatus.OK).end();
 };
 
 export { uploadFile, deleteFile };
--- middlewares/upload.middleware.js
+++ middlewares/upload.middleware.js
@@ -1,32 +1,6 @@
-import fs from 'fs';
 import multer from 'multer';
 
-const MIME_EXTENSIONS = {
-  'image/jpeg': '.jpg',
-  'image/png': '.png',
-  'image/gif': '.gif',
-  'image/webp': '.webp',
-  'image/svg+xml': '.svg',
-};
+const storage = multer.memoryStorage();
+const uploadMiddleware = multer({ storage });
 
-const storage = multer.diskStorage({
-  destination: function (req, file, calback) {
-    let path = `public/uploads/`;
-
-    if (req.body.category === 'forum') {
-      path += `forum/`;
-    } else {
-      path += `user/${req.user.id}`;
-    }
-
-    fs.mkdirSync(path, { recursive: true });
-
-    calback(null, path);
-  },
-  filename: function (req, file, calback) {
-    calback(null, `${Date.now()}${MIME_EXTENSIONS[file.mimetype] ?? ''}`);
-  }
-});
-const upload = multer({ storage });
-
-export default upload;
+export default uploadMiddleware;
